dd21b9b3da8ac426afcb7538517a9643f60f18f1
kz04px <6536046+kz04px@users.noreply.github.com>
2019-03-15 03:08:48 +0000

    Scale eval values
    Hundredths of a stone

--- src/eval.hpp
+++ src/eval.hpp
@@ -10,7 +10,7 @@ int eval(const Position &pos) {
     const bool them = !us;
     const int num_us = popcountll(pos.pieces[us]);
     const int num_them = popcountll(pos.pieces[them]);
-    return num_us - num_them;
+    return 100 * (num_us - num_them);
 }
 
 #endif
