434f9176412e6ae4768159190f4fe00f89c2eb82
kz04px <6536046+kz04px@users.noreply.github.com>
2022-05-18 14:57:14 +0100

    Add eval.cpp

---
+++ src/search/eval.cpp
@@ -0,0 +1,8 @@
+#include "eval.hpp"
+#include <libataxx/position.hpp>
+
+int eval(const libataxx::Position &pos) {
+    const int num_us = pos.us().count();
+    const int num_them = pos.them().count();
+    return 100 * (num_us - num_them);
+}
--- src/search/eval.hpp
+++ src/search/eval.hpp
@@ -1,13 +1,11 @@
 #ifndef EVAL_HPP
 #define EVAL_HPP
 
-#include <libataxx/position.hpp>
+namespace libataxx {
+class Position;
+}  // namespace libataxx
 
 // Return the evaluation of the position from the side to move's point of view
-int eval(const libataxx::Position &pos) {
-    const int num_us = pos.us().count();
-    const int num_them = pos.them().count();
-    return 100 * (num_us - num_them);
-}
+int eval(const libataxx::Position &pos);
 
 #endif
